2f402221adb0c1b7f3ed326597be44e2495aa2aa
z1yodullayev <ergash.king2@gmail.com>
2022-07-28 16:06:46 +0500

    commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var intro = alert("Assalomu Aleykum Xush kelibsiz");
+console.log("F.I.SH: " + prompt("F.I.SH"));
+console.log("Yoshi: " + prompt("Yoshingiz nechida"));
+console.log("Tug'ilgan joyingiz: " + prompt("Tug'ilgan joyingiz"));
+console.log("Qayerda ta'lim olish " + prompt("Qayerda ta'lim olasiz,"));
+console.log("Telefon raqami: " + prompt("Telefon raqamingiz"));
+console.log("oilangiz haqida malumot" + prompt("oila haqida malumot"));
+console.log("Yashash manzili: " + prompt("Yashash manzilingiz"));
